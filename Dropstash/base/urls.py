929d9944dd644258509ed86a8ee6e2b140067a17
Mikhail Volynov <st3althtech@mail.ru>
2017-06-29 01:02:20 +0300

    Added basic view support plus landing view

---
+++ Dropstash/base/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'base'
+urlpatterns = [
+    url(r'^$', views.LandingView.as_view(), name='home'),
+]
--- Dropstash/base/views.py
+++ Dropstash/base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class PageView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super(PageView, self).get_context_data(**kwargs)
+        return context
+
+
+class LandingView(PageView):
+    template_name = 'base/home.html'
