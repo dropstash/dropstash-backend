dc7254c0b0f4dbf2a3ed0fbc4f685bb586aee832
Mikhail Volynov <st3althtech@mail.ru>
2017-07-02 17:16:39 +0300

    [WIP] Restructured models draw

--- Dropstash/base/models.py
+++ Dropstash/base/models.py
@@ -3,13 +3,15 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from datetime import datetime
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, verbose_name='user', on_delete=models.CASCADE, primary_key=True)
     avatar = models.ImageField(verbose_name='avatar')
-    # Description?
-    # PageReferences?
-    # Saved quotes?
+    
+    # @property
+    # def stash(self):
 
 
 @receiver(post_save, sender=User)
@@ -22,44 +24,89 @@ def create_user_profile(sender, instance, created, **kwargs):
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
-# class Hyperlink(models.Model):
-#     target_url = models.URLField(max_length=200, verbose_name='hyperlink')
-#     comment = models.TextField(verbose_name='comment')
-#     picture = models.ImageField(verbose_name='picture')
+
+class Post(models.Model):
+    author = models.ForeignKey(User, verbose_name='author', on_delete=models.CASCADE)
+
+    link = models.URLField(verbose_name='hyperlink')
+    headline = models.CharField(max_length=200, verbose_name='headline')
+    description = models.TextField(verbose_name='comment')
+    content = models.TextField(verbose_name='quote')
+    preview_picture = models.ImageField(verbose_name='preview picture', blank=True)
+
+    clusters = models.ManyToManyField(Cluster, verbose_name='used in clusters')
+
+    submission_datetime = models.DateTimeField(verbose_name='submission datetime', default=datetime.now, blank=True)
+    is_published = models.BooleanField(default=False, blank=True, verbose_name='publication marker')
+    publication_datetime = models.DateTimeField(verbose_name='publication datetime', default=datetime.now, blank=True)
+
+    def publish(self):
+        if not self.is_published:
+            self.is_published = True
+            self.publication_datetime = datetime.now()
+            # TODO: also reposts
+
+    def conceal(self):
+        if self.is_published:
+            self.is_published = False
+            # TODO: also reposts
 
 
-# class Post(models.Model):
-#     author = models.ForeignKey(User, verbose_name='author', on_delete=models.CASCADE)
-#     collection = models.ForeignKey(Collection, verbose_name='collection', on_delete=models.CASCADE)
-#     link = models.ForeignKey(Hyperlink, verbose_name='hyperlink', on_delete=models.CASCADE)
-#     message = models.TextField(verbose_name='author comment')
-#     submission_datetime = models.DateTimeField(verbose_name='submission datetime', default=datetime.now, blank=True)
-#     is_published = models.BooleanField(default=False, blank=True, verbose_name='publication marker')
-#
-#     # invalid logic
-#     publication_datetime = models.DateTimeField(verbose_name='publication datetime', default=datetime.now, blank=True)
+class PostComment(models.Model):
+    post = models.ForeignKey(Post, verbose_name='linked post', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name='linked user', on_delete=models.CASCADE)
+    message = models.TextField(verbose_name='comment')
 
 
-# class PostComment(models.Model):
-#     post = models.ForeignKey(Post, verbose_name='post', on_delete=models.CASCADE)
+class PostVoteUp(models.Model):
+    user = models.ForeignKey(User, verbose_name='linked user', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, verbose_name='linked post')
+
+    class Meta:
+        unique_together = ('user', 'post',)
 
 
-# class Repost(models.Model):
-#     author = models.ForeignKey(User, verbose_name='author', on_delete=models.CASCADE)
-#     post = models.ForeignKey(Post, verbose_name='post', on_delete=models.CASCADE)
-#     publication_datetime = models.DateTimeField(verbose_name='publication datetime', default=datetime.now, blank=True)
+class PostVoteDown(models.Model):
+    user = models.ForeignKey(User, verbose_name='linked user', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, verbose_name='linked post')
+
+    class Meta:
+        unique_together = ('user', 'post',)
 
 
-# class PageReference(models.Model):
-#     title = models.CharField(max_length=200, verbose_name='title')
-#     description = models.TextField(verbose_name='description')
-#     picture = models.ImageField(verbose_name='picture')
-#     author = models.ForeignKey(User, verbose_name='author', on_delete=models.CASCADE)
-#     # Posts and reposts?
-#     submission_datetime = models.DateTimeField(verbose_name='submission datetime', default=datetime.now, blank=True)
+class Repost(models.Model):
+    user = models.ForeignKey(User, verbose_name='reposted by', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, verbose_name='post')
+    publication_datetime = models.DateTimeField(verbose_name='publication datetime', default=datetime.now, blank=True)
 
-#
-# class Collection(models.Model):
-#     pass
-#     # PageReference? layout?
+
+class Cluster(models.Model):
+    headline = models.CharField(max_length=200, verbose_name='headline')
+    content = models.TextField(verbose_name='cluster overlay')
+
+
+class ClusterComment(models.Model):
+    cluster = models.ForeignKey(Cluster, 'linked cluster', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, 'linked user', on_delete=models.CASCADE)
+    message = models.TextField(verbose_name='comment')
+
+
+class ClusterVoteUp(models.Model):
+    user = models.ForeignKey(User, verbose_name='linked user', on_delete=models.CASCADE)
+    cluster = models.ForeignKey(Cluster, verbose_name='linked post')
+
+    class Meta:
+        unique_together = ('user', 'cluster',)
+
+
+class ClusterVoteDown(models.Model):
+    user = models.ForeignKey(User, verbose_name='linked user', on_delete=models.CASCADE)
+    cluster = models.ForeignKey(Cluster, verbose_name='linked post')
+
+    class Meta:
+        unique_together = ('user', 'cluster',)
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=200, verbose_name='tag')
 
